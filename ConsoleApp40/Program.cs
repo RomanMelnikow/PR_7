189c666cb9ae88d049def87f903f0fc4eca19592
RomanMelnikow <119843583+RomanMelnikow@users.noreply.github.com>
2023-02-18 13:54:04 +0300

    Add files via upload

---
+++ ConsoleApp40/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+var fs = default(FileStream);
+try
+{
+    // Opens a text tile.
+    fs = new FileStream(@"C:\1111\data.txt", FileMode.Open);
+    var sr = new StreamReader(fs);
+
+    // A value is read from the file and output to the console.
+    string? line = sr.ReadLine();
+    Console.WriteLine(line);
+}
+catch (FileNotFoundException e)
+{
+    Console.WriteLine($"[Data File Missing] {e}");
+    throw new FileNotFoundException(@"[data.txt not in c:\1111 directory]", e);
+}
+finally
+{
+    if (fs != null)
+        fs.Close();
+}
